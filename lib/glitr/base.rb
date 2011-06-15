2cc1a34e16e09d493054954b242e8e03dccf4573
Sam Umbach <sam@thinkrelevance.com>
2011-06-14 23:02:33 -0400

    Move Glitr::Base class methods into a class << self block

--- lib/glitr/base.rb
+++ lib/glitr/base.rb
@@ -9,118 +9,128 @@ module Glitr
       self.id, self.attributes = id, attributes
     end
 
-    def self.entity_type
-      @entity_type ||= name.split("::").last
-    end
-
-    def self.entity_type=(type)
-      @entity_type = type
-    end
-
-    def self.namespace
-      @namespace
-    end
-
-    def self.namespace=(ns)
-      @namespace = ns
-    end
-
     class << self
+      def entity_type
+        @entity_type ||= name.split("::").last
+      end
+
+      def entity_type=(type)
+        @entity_type = type
+      end
+
+      def namespace
+        @namespace
+      end
+
+      def namespace=(ns)
+        @namespace = ns
+      end
+
       alias :set_entity_type :entity_type=
       alias :set_namespace :namespace=
-    end
 
-    def self.all(conditions = {})
-      query = <<-QUERY
-        PREFIX :   <http://metrumrg.com/metamodl/>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      def all(conditions = {})
+        query = <<-QUERY
+          PREFIX :   <http://metrumrg.com/metamodl/>
+          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+          PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-        SELECT DISTINCT ?_subject ?_predicate ?_object
-        WHERE {
-         ?#{entity_type} rdf:type :#{entity_type} .
+          SELECT DISTINCT ?_subject ?_predicate ?_object
+          WHERE {
+           ?#{entity_type} rdf:type :#{entity_type} .
 
-         #{ build_filters entity_type, conditions  }
+           #{ build_filters entity_type, conditions  }
 
-         LET ( ?_subject := ?#{entity_type} ) .
-         ?_subject ?_predicate ?_object .
-        }
-      QUERY
+           LET ( ?_subject := ?#{entity_type} ) .
+           ?_subject ?_predicate ?_object .
+          }
+        QUERY
 
-      subjects = connection.fetch_subjects(query)
-      build_all(subjects)
-    end
+        subjects = connection.fetch_subjects(query)
+        build_all(subjects)
+      end
 
-    def self.select(attributes, conditions = {})
-      query = <<-QUERY
-        PREFIX :   <http://metrumrg.com/metamodl/>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      def select(attributes, conditions = {})
+        query = <<-QUERY
+          PREFIX :   <http://metrumrg.com/metamodl/>
+          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+          PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-        SELECT DISTINCT #{ attributes.map {|attr| "?#{attr}"}.join(' ') }
-        WHERE {
-          ?#{entity_type} rdf:type :#{entity_type} .
-          #{ build_bindings entity_type, attributes }
-          #{ build_filters  entity_type, conditions }
-        }
-      QUERY
+          SELECT DISTINCT #{ attributes.map {|attr| "?#{attr}"}.join(' ') }
+          WHERE {
+            ?#{entity_type} rdf:type :#{entity_type} .
+            #{ build_bindings entity_type, attributes }
+            #{ build_filters  entity_type, conditions }
+          }
+        QUERY
 
-      result = connection.fetch(query)
-    end
+        result = connection.fetch(query)
+      end
 
-    def self.count(conditions = {})
-      query = <<-QUERY
-        PREFIX :   <http://metrumrg.com/metamodl/>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      def count(conditions = {})
+        query = <<-QUERY
+          PREFIX :   <http://metrumrg.com/metamodl/>
+          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+          PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-        SELECT (count(distinct ?uuid) as ?count)
-        WHERE {
+          SELECT (count(distinct ?uuid) as ?count)
+          WHERE {
 
-          ?#{entity_type} rdf:type :#{entity_type};
-                  :uuid ?uuid .
-          #{ build_filters entity_type, conditions }
-        }
-      QUERY
+            ?#{entity_type} rdf:type :#{entity_type};
+                    :uuid ?uuid .
+            #{ build_filters entity_type, conditions }
+          }
+        QUERY
 
-      result = connection.fetch(query)
-      result.first && result.first['count'].to_i
-    end
+        result = connection.fetch(query)
+        result.first && result.first['count'].to_i
+      end
 
-    def self.columns
-      return @columns if defined?(@columns)
+      def columns
+        return @columns if defined?(@columns)
 
-      namespace = "http://metrumrg.com/metamodl/"
-      query = <<-QUERY
-        PREFIX :   <#{namespace}>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        namespace = "http://metrumrg.com/metamodl/"
+        query = <<-QUERY
+          PREFIX :   <#{namespace}>
+          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+          PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-        SELECT DISTINCT ?column
-        WHERE {
+          SELECT DISTINCT ?column
+          WHERE {
 
-          ?#{entity_type} rdf:type :#{entity_type};
-                  ?column ?_ .
-        }
-      QUERY
+            ?#{entity_type} rdf:type :#{entity_type};
+                    ?column ?_ .
+          }
+        QUERY
 
-      result = connection.fetch(query)
-      @columns = result.
-        select {|row| row['column'].match(/^#{namespace}/)}.
-        map {|row| row['column'].sub(/^#{namespace}/,"") }
-    end
+        result = connection.fetch(query)
+        @columns = result.
+          select {|row| row['column'].match(/^#{namespace}/)}.
+          map {|row| row['column'].sub(/^#{namespace}/,"") }
+      end
 
 
-    def self.build_filters(entity_type, conditions)
-      conditions.reject! {|attr, value| value.blank?}
+      def build_filters(entity_type, conditions)
+        conditions.reject! {|attr, value| value.blank?}
 
-      bindings = build_bindings entity_type, conditions.keys
-      filters = conditions.map{|attr, *values| "FILTER ( ?#{attr} in (#{values.flatten.map {|val| '"'+val+'"'}.join(',') }) ) ." }.join("\n")
-      [bindings, filters].join("\n")
-    end
+        bindings = build_bindings entity_type, conditions.keys
+        filters = conditions.map{|attr, *values| "FILTER ( ?#{attr} in (#{values.flatten.map {|val| '"'+val+'"'}.join(',') }) ) ." }.join("\n")
+        [bindings, filters].join("\n")
+      end
 
-    def self.build_bindings(entity_type, attributes)
-      attributes.map{|attr| "OPTIONAL { ?#{entity_type} :#{attr} ?#{attr} . }" }.join("\n")
+      def build_bindings(entity_type, attributes)
+        attributes.map{|attr| "OPTIONAL { ?#{entity_type} :#{attr} ?#{attr} . }" }.join("\n")
+      end
+
+      private
+
+      def connection
+        @connection ||= Glitr::Connection.new(:service => "metamodl_#{Rails.env}")
+      end
+
+      def build_all(entities)
+        entities.map {|id, attrs| new(id, attrs) }
+      end
     end
 
     def [](attr)
@@ -142,14 +152,6 @@ module Glitr
     end
 
     private
-
-    def self.connection
-      @connection ||= Glitr::Connection.new(:service => "metamodl_#{Rails.env}")
-    end
-
-    def self.build_all(entities)
-      entities.map {|id, attrs| new(id, attrs) }
-    end
 
     def namespaced_key(key)
       "#{self.class.namespace}/#{key}"
